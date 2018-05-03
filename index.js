3682a0e1c58968564ccfaf246836ea9ae5f41525
Michael Skolnik <mikeskolnik@gmail.com>
2018-05-03 16:46:47 -0400

    first run of changes

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ controller.setupWebserver(process.env.PORT, function (err, webserver) {
 controller.on('slash_command', function (slashCommand, message) {
 
     switch (message.command) {
-        case "/echo": //handle the `/echo` slash command. We might have others assigned to this app too!
+        case "/support": //handle the `/echo` slash command. We might have others assigned to this app too!
             // The rules are simple: If there is no text following the command, treat it as though they had requested "help"
             // Otherwise just echo back to them what they sent us.
 
@@ -98,18 +98,34 @@ controller.on('slash_command', function (slashCommand, message) {
             // if no text was supplied, treat it as a help command
             if (message.text === "" || message.text === "help") {
                 slashCommand.replyPrivate(message,
-                    "I echo back what you tell me. " +
-                    "Try typing `/echo hello` to see.");
+                    "Please provide the following information: " +
+                    "Patient ID:" + 
+                    "Description of problem:" + 
+                    "What clinician sees:" + 
+                    "What patient sees:" + 
+                    "What you've already tried to fix it:");
                 return;
             }
 
-            // If we made it here, just echo what the user typed back at them
-            //TODO You do it!
-            slashCommand.replyPublic(message, "1", function() {
-                slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
-            });
-
+    switch (message.command) {
+        case "/file": //handle the `/echo` slash command. We might have others assigned to this app too!
+            // The rules are simple: If there is no text following the command, treat it as though they had requested "help"
+            // Otherwise just echo back to them what they sent us.
+        
+            // but first, let's make sure the token matches!
+            if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
+        
+            // if no text was supplied, treat it as a help command
+            if (message.text === "" || message.text === "help") {
+                slashCommand.replyPrivate(message,
+                    "Please provide the following information: " +
+                    "Patient ID the file was sent to:" + 
+                    "Patient ID of the patient whose information was in it (if applicable):" + 
+                    "File name(s):");
+                return;  
+            }      
             break;
+
         default:
             slashCommand.replyPublic(message, "I'm afraid I don't know how to " + message.command + " yet.");
 
